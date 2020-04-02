b2e682457656c2e1d59fa69637ade0ec13cab172
snyfery <55048402+snyfery@users.noreply.github.com>
2020-04-02 17:49:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.nav-link, .navbar-brand, .new-button').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 1500);
+});
